f9b81ed9ca966a14560fb9739d86e1f3b810c4bd
JT <547158+jntrnr@users.noreply.github.com>
2023-08-03 10:22:08 +1200

    Add a few fixes to benchmarks too

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,22 +1,21 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use truffle::{Evaluator, FunctionId, Parser, Translater, TypeChecker};
+use truffle::{Evaluator, FunctionId, Lexer, Parser, Translater, TypeChecker};
 
 #[cfg(feature = "async")]
 fn run_file(filename: &str) {
     use futures::executor::block_on;
-    use truffle::Lexer;
 
     let contents = std::fs::read(filename).expect("couldn't find file");
 
     let mut lexer = Lexer::new(contents.clone(), 0);
 
-    let tokens = lexer.lex();
+    let tokens = lexer.lex().unwrap();
     let mut parser = Parser::new(tokens, contents, 0);
-    parser.parse();
+    parser.parse().unwrap();
 
     let mut typechecker = TypeChecker::new(parser.results);
-    typechecker.typecheck();
+    typechecker.typecheck().unwrap();
 
     let mut translater = Translater::new(typechecker);
 
@@ -33,21 +32,24 @@ fn run_file(filename: &str) {
 fn run_file(filename: &str) {
     let contents = std::fs::read(filename).expect("couldn't find file");
 
-    let mut parser = Parser::new(contents, 0, 0);
-    parser.parse();
+    let mut lexer = Lexer::new(contents.clone(), 0);
 
-    let mut typechecker = TypeChecker::new();
-    typechecker.typecheck(&parser.results);
+    let tokens = lexer.lex().unwrap();
+    let mut parser = Parser::new(tokens, contents, 0);
+    parser.parse().unwrap();
 
-    let mut translater = Translater::new();
+    let mut typechecker = TypeChecker::new(parser.results);
+    typechecker.typecheck().unwrap();
+
+    let mut translater = Translater::new(typechecker);
 
     #[allow(unused_mut)]
-    let mut output = translater.translate(&parser.results, &typechecker);
+    let mut output = translater.translate();
 
     let mut evaluator = Evaluator::default();
     evaluator.add_function(output);
 
-    let _ = evaluator.eval(FunctionId(0), &typechecker.functions);
+    let _ = evaluator.eval(FunctionId(0), &translater.typechecker.functions);
 }
 
 fn parser_benchmark(c: &mut Criterion) {
